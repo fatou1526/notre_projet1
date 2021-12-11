3b25cb91771e1de40f77488b7bd69b94ea6047a6
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2021-12-11 10:48:48 +0000

    Definition of some methods

---
+++ bank.py
@@ -0,0 +1,11 @@
+class Account:
+    def __init__(self, num_account, amount):
+        self.__num_account = num_account
+        self.__amount = amount
+    def open_account(self):
+        print("Welcome, you will not be deceive for choosing our bank branch")
+    def deposit(self):
+        print("Please, give your account number")
+    def withdrawal(self):
+        print("Please, fill the form with your account number, your ID and the amount to withdraw")
+    
