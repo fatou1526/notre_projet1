5bde0992969a1ecb3ca79f071b4104d3fa6fe827
Sana Massaly <massalysana@gmail.com>
2021-12-11 20:20:38 +0000

    Mis en place des focntions de la bank s en page dea#

--- mybank.py
+++ mybank.py
@@ -4,12 +4,15 @@ class BankAccount:
         self.client_id = client_id 
         self.__num_account = num_account
         self.__amount = amount
+
      @property
      def num_account(self):
          return f"{self.__num_account}"
+
      @num_account.setter
      def num_account(self, num_account):
-         assert isinstance(num_account, int), "Give me an integer, not a %r!" % type(num_account)   
+         assert isinstance(num_account, int), "Give me an integer, not a %r!" % type(num_account) 
+
     def open_account(self):
         """this method opens a new account for a client"""
         print("Welcome, you will not be deceived for choosing our bank branch")
@@ -20,8 +23,7 @@ class BankAccount:
     def deposit(self):
         """This method deposit the specified amount into the client's account"""
         print("Please, give your account number")
-        
-        
+          
     def withdrawal(self):
         """This method withdraws the specified amount from client's account"""
         print("Please, fill the form with your account number, your ID and the amount to withdraw")
