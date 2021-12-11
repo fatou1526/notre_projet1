0b55bf0c08f4d2444606e2c4771a010a8616902b
Mamadou KEBE <maskebe@gmail.com>
2021-12-11 11:38:08 +0000

    Added documentation to the methods

--- bank.py
+++ bank.py
@@ -1,12 +1,18 @@
-class Account:
+class BankAccount:
     """This class simulates a bank account for a client"""
     def __init__(self, num_account, amount):
         self.__num_account = num_account
         self.__amount = amount
+        
     def open_account(self):
+        """this method opens a new account for a client"""
         print("Welcome, you will not be deceive for choosing our bank branch")
+        
     def deposit(self):
+        """This method deposit the specified amount into the client's account"""
         print("Please, give your account number")
+        
     def withdrawal(self):
+        """This method withdraws the specified amount from client's account"""
         print("Please, fill the form with your account number, your ID and the amount to withdraw")
     
